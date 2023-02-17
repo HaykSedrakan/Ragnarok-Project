139794301d6f1148538e4b669c03cb7f59e235d8
HaykSedrakan <118746658+HaykSedrakan@users.noreply.github.com>
2023-02-17 23:44:43 +0400

    Add files via upload

---
+++ src/assets/js/langauges.js
@@ -0,0 +1,18 @@
+export const languages = {
+    en:{
+        editions:'Editions',
+        controller:'Controller',
+        about:'About',
+        explore:'Explore',
+        news:'News',
+        faq:'Faq'
+    },
+    ru:{
+        editions:'Издания',
+        controller:'Контроллер',
+        // about:'Об игре',
+        explore:'Исследовать',
+        news:'Новости',
+        faq:'Часто задаваемые вопросы'
+    }
+}
